48f97ed168948d18b216104898c0c31db0a87a82
maniusz28@wp.pl <maniusz28@wp.pl>
2018-07-25 20:06:54 +0200

    Solution based on line fitting

--- Line detection.py
+++ Line detection.py
@@ -4,13 +4,89 @@ import sys
 import math
 
 #video = cv2.VideoCapture(0) ----- for camera read
-video = cv2.VideoCapture("Cut films/7.mp4")
+video = cv2.VideoCapture("Cut films/5.mp4")
 
 cv2.namedWindow('binary frame')
 cv2.createTrackbar('H', 'binary frame', 20, 174, lambda x: None)
 cv2.setTrackbarMin('H', 'binary frame', 5)
 
 video_read_correctly, frame = video.read()
+
+def fit_lines_to_contour(contour):
+    lines = []
+    for index in range(0, len(contour), 10):
+        vx, vy, x, y = cv2.fitLine(contour[index : index + 10], cv2.DIST_L2, 0, 0.01, 0.01)
+        vx, vy, x, y = float(vx), float(vy), float(x), float(y)
+        lines.append(dict(
+            {
+                "points": [(x, y)],
+                "vx": vx,
+                "vy": vy
+            })
+        )
+
+    """ if len(lines) > 0:
+        cv2.circle(frame, (int(lines[0]["points"][0][0]), int(lines[0]["points"][0][1])), 2, (0,0,255), -1)
+        cv2.circle(frame, (int(lines[0]["points"][0][0]), int(lines[0]["a"] * lines[0]["points"][0][0] + lines[0]["b"])), 2, (0,0,255), -1)
+        print(int(lines[0]["points"][0][1]))
+        print(int(lines[0]["a"] * lines[0]["points"][0][0] + lines[0]["b"]))
+        cv2.imshow("binary frame", frame)
+        if cv2.waitKey(500) & 0xFF == ord('q'):
+            pass
+        input("INPUT") """
+    labeled_group = []
+    for line in lines:
+        if line["vx"] != 0:
+            lines.remove(line)
+            labeled_group.append(line)
+            break
+    if len(labeled_group) == 0:
+        return
+    for line in lines:
+        if line["vx"] == 0:
+            continue
+        labeled = False
+        min_distance = sys.maxsize
+        temp = dict({})
+        for labeled_line in labeled_group:
+            n1 = (line["points"][0][0] - labeled_line["points"][0][0]) / labeled_line["vx"]
+            n2 = (labeled_line["points"][0][0] - line["points"][0][0]) / line["vx"]
+            d1 = abs((labeled_line["points"][0][1] + n1 * labeled_line["vy"]) - line["points"][0][1])
+            d2 = abs((line["points"][0][1] + n2 * line["vy"]) - labeled_line["points"][0][1])
+            d_mean = (d1 + d2) / 2
+            #if d1 <= min([10, min_distance]) and d2 <= min([10, min_distance]):
+            if d_mean <= min([10, min_distance]):
+                temp = labeled_line
+                #min_distance = min([d1, d2])
+                min_distance = d_mean
+                labeled = True
+        if not labeled:
+            labeled_group.append(line)
+        else:
+            if len(temp["points"]) == 1:
+                temp["points"].append(temp["points"][0])
+            temp["points"].append(line["points"][0])
+            first_point = np.array([temp["points"][0][0], temp["points"][0][1]], dtype=float)
+            second_point = np.array([line["points"][0][0], line["points"][0][1]], dtype=float)
+            points = np.array([first_point, second_point])
+            vx, vy, x, y = cv2.fitLine(points, cv2.DIST_L2, 0, 0.01, 0.01)
+            vx, vy, x, y = float(vx), float(vy), float(x), float(y)
+            temp["points"][0] = (x, y)
+            temp["vx"] = vx
+            temp["vy"] = vy
+
+    labeled_group = [labeled_line for labeled_line in labeled_group if len(labeled_line["points"]) > 1]
+    
+    for labeled_line in labeled_group:
+        if len(labeled_line["points"]) > 1:
+            for points in labeled_line["points"][1:]:
+                cv2.circle(frame, (int(points[0]), int(points[1])), 2, (255,255,255), -1)
+            cv2.imshow("binary frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+            input("INPUT")
+    
 
 #x = cv2.createHausdorffDistanceExtractor()
 
@@ -24,12 +100,12 @@ while(video_read_correctly):
         
     extracted_color_frame = cv2.inRange(hsv_frame, lower_H, upper_H)
 
-    _, contours, _ = cv2.findContours(extracted_color_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(extracted_color_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
 
     filtered_contours = []
     rectangles = []
     rectangles_vertices = []
-
+    cv2.drawContours(frame, contours, -1, (0, 0, 255), 1)
     for contour in contours:
 
         contour_area = cv2.contourArea(contour)
@@ -38,8 +114,9 @@ while(video_read_correctly):
 
             hull = cv2.convexHull(contour, _, True)
             epsilon = 0.01 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(hull, epsilon, True)
-            
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            fit_lines_to_contour(contour)
+
             rect = cv2.minAreaRect(approx)
             box = cv2.boxPoints(rect)
             box = np.int32(box)
@@ -117,7 +194,7 @@ while(video_read_correctly):
 
     cv2.imshow("binary frame", frame)
 
-    if cv2.waitKey(200) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
     video_read_correctly, frame = video.read()
