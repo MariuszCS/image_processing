513d59ff522cf461f9b1b34a230a1da0ae0254fc
maniusz28@wp.pl <maniusz28@wp.pl>
2018-07-29 21:33:22 +0200

    old code removed

--- Line detection.py
+++ Line detection.py
@@ -98,7 +98,7 @@ def contour_to_lines(contour):
         # consider only groups consisting of at least 3 elements
         # the first element is the mean point of the group
         if len(labeled_line["points"][1:]) > 2:
-            # 0.6 is aprrox tan 45 degrees
+            # 1 is tan 45 degrees
             if abs(labeled_line["vy"] / labeled_line["vx"]) > 1:
                 vertical_lines.append(labeled_line)
             else:
@@ -113,6 +113,7 @@ def contour_to_lines(contour):
             break
         input() """
     return vertical_lines, horizontal_lines
+    # here comes code for lines pairing, not sure if this process is necessary
     """ line_pairs = []
     start_index = 0
     indices_taken = []
@@ -196,15 +197,6 @@ def find_trajectory(lines):
         n2 = (frame.shape[0] - y_middle) / vy
         x_top = x_mean + n1 * vx
         x_bottom = x_mean + n2 * vx
-        #cv2.line(frame, line_left["points"][0], line_right["points"][0], (255, 0, 0), 2) 
-        #input()
-        """ points = np.array([line_left["points"] + line_right["points"]])
-        vx, vy, x, y = cv2.fitLine(points, cv2.DIST_L2, 0, 0.01, 0.01)
-        vx, vy, x, y = float(vx), float(vy), float(x), float(y)
-        n1 = -y / vy
-        n2 = (frame.shape[0] - y) / vy
-        x1 = x + n1 * vx
-        x2 = x + n2 * vx"""
         cv2.circle(frame, (int(x_mean), int(y_middle)), 10, (255, 0, 0), -1)
         cv2.circle(frame, (int(x_left), int(y_middle)), 10, (0, 255, 0), -1)
         cv2.circle(frame, (int(x_right), int(y_middle)), 10, (0, 0, 255), -1)
@@ -224,9 +216,6 @@ while(video_read_correctly):
 
     _, contours, _ = cv2.findContours(extracted_color_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
 
-    """ filtered_contours = []
-    rectangles = []
-    rectangles_vertices = [] """
     varitcal_lines = []
     #cv2.drawContours(frame, contours, -1, (0, 0, 255), 1)
     for contour in contours:
@@ -236,94 +225,12 @@ while(video_read_correctly):
             varitcal_lines += contour_to_lines(contour)[0]
 
     find_trajectory(varitcal_lines)
-    input()
     cv2.imshow("binary frame", frame)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
     video_read_correctly, frame = video.read()
-"""             hull = cv2.convexHull(contour, _, True)
-            epsilon = 0.01 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
-            
-
-            rect = cv2.minAreaRect(approx)
-            box = cv2.boxPoints(rect)
-            box = np.int32(box)
-            height, width = rect[1]
-            #if cv2.matchShapes(np.array([box]), contour, cv2.CONTOURS_MATCH_I3, 0) < 5:
-            #if (x.computeDistance(cv2.convexHull(contour, _, True), np.array([box]))) < 20:
-
-            if (height > width * 5 or width > height * 5):
-            #and cv2.contourArea(np.array([box])) <= 2 * cv2.contourArea(hull):
-                rectangles.append(rect)
-                filtered_contours.append(contour)
-                rectangles_vertices.append(np.array([box]))
-
-    cv2.drawContours(frame, filtered_contours, -1, (0, 0, 255), 1)
-    cv2.drawContours(frame, rectangles_vertices, -1, (0, 255, 0), 1)
-
-    lines = []
-
-    for rectangle in rectangles:
-
-        center_x, center_y = rectangle[0]
-        height, width = rectangle[1]
-        angle = rectangle[2]
-
-        if height > width:
-            x1 = int(center_x - abs((height / 2) * math.cos(math.radians(angle))))
-            y1 = int(center_y + abs((height / 2) * math.sin(math.radians(angle))))
-            x2 = int(center_x + abs((height / 2) * math.cos(math.radians(angle))))
-            y2 = int(center_y - abs((height / 2) * math.sin(math.radians(angle))))
-        else:
-            x1 = int(center_x + abs((width / 2) * math.sin(math.radians(angle))))
-            y1 = int(center_y + abs((width / 2) * math.cos(math.radians(angle))))
-            x2 = int(center_x - abs((width / 2) * math.sin(math.radians(angle))))
-            y2 = int(center_y - abs((width / 2) * math.cos(math.radians(angle))))
-
-        lines.append(((x1, y1), (x2, y2)))
-
-        #cv2.line(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
-       
-    middle_point_x = int(frame.shape[1] / 2)
-    closest_left_line = tuple()
-    closest_right_line = tuple()
-
-    min_distance_left = -sys.maxsize
-    min_distance_right = sys.maxsize
-
-    for line in lines:
-        if (line[0][0] <= middle_point_x and line[1][0] <= middle_point_x) \
-        and (line[0][0] >= min_distance_left or line[1][0] >= min_distance_left):
-            if line[0][0] > line[1][0]:
-                min_distance_left = line[0][0]
-            else:
-                min_distance_left = line[1][0]
-            closest_left_line = line
-        elif (line[0][0] >= middle_point_x and line[1][0] >= middle_point_x) \
-        and (line[0][0] <= min_distance_right or line[1][0] <= min_distance_right):
-            if line[0][0] < line[1][0]:
-                min_distance_right = line[0][0]
-            else:
-                min_distance_right = line[1][0]
-            closest_right_line = line
-
-    cv2.line(frame, (middle_point_x, frame.shape[0]), (middle_point_x, 0), (0, 0, 255), 2)
-
-    if len(closest_left_line) > 0:
-        cv2.line(frame, closest_left_line[0], closest_left_line[1], (255, 0, 0), 2)
-
-    if len(closest_right_line) > 0:
-        cv2.line(frame, closest_right_line[0], closest_right_line[1], (255, 0, 0), 2)
-
-    if len(closest_left_line) > 0 and len(closest_right_line) > 0:
-        cv2.line(frame, (int((closest_right_line[0][0] + closest_left_line[0][0] + closest_right_line[1][0] + closest_left_line[1][0]) / 4), 0)\
-        , (int((closest_right_line[0][0] + closest_left_line[0][0] + closest_right_line[1][0] + closest_left_line[1][0]) / 4), frame.shape[0]),\
-        (255, 0, 0), 2) 
-    """
-
     
 video.release()
 cv2.destroyAllWindows()
