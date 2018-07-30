37395abde8bed4c4438aad689f726450847b1417
maniusz28@wp.pl <maniusz28@wp.pl>
2018-07-30 21:04:41 +0200

    fixes for checking if line belongs to group, and dashed line grouping

--- Line detection.py
+++ Line detection.py
@@ -12,20 +12,24 @@ cv2.setTrackbarMin('H', 'binary frame', 5)
 
 video_read_correctly, frame = video.read()
 
-def check_group_for_line(line, group, min_distance):
+def check_group_for_line(line, group, min_distance, max_distance):
+    # the distance between the y of the line and the y (for the same x) of the group is calculated
+    # if the distance (error) is small enough (<5) than the line is considered to match the group
+    # the distance is calculated in both ways (from the line to group and from group to the line)
+    # and the mean value is taken as the final distance
     # n is the ratio from the following equation
     # x2 = x1 + n * vx
     # y2 = y1 + n * vy
     # where (x1, y1) is point that belongs to the group and (x2, y2) to the line we want to classify
     n1 = (line["points"][0][0] - group["points"][0][0]) / group["vx"]
-    #n2 = (labeled_line["points"][0][0] - line["points"][0][0]) / line["vx"]
+    n2 = (group["points"][0][0] - line["points"][0][0]) / line["vx"]
     # from the above equations the distance is the absolute value from (y1 - y2)
     d1 = abs((group["points"][0][1] + n1 * group["vy"]) - line["points"][0][1])
-    #d2 = abs((line["points"][0][1] + n2 * line["vy"]) - labeled_line["points"][0][1])
-    #d_mean = (d1 + d2) / 2
+    d2 = abs((line["points"][0][1] + n2 * line["vy"]) - group["points"][0][1])
+    d_mean = (d1 + d2) / 2
     # if d1 <= min([10, min_distance]) and d2 <= min([10, min_distance]):  ?
-    # if d_mean <= min([5, min_distance]): ?
-    if d1 <= min([5, min_distance]):
+    if d_mean <= min([max_distance, min_distance]): 
+    #if d1 <= min([max_distance, min_distance]):
         #min_distance = min([d1, d2])
         min_distance = d1
         return d1, True
@@ -35,7 +39,7 @@ def check_group_for_line(line, group, min_distance):
 
 def determine_lines_for_contour(contour):
     lines = []
-    # fits line to every 8 points of the contour
+    # fits line to every 10 points of the contour
     for index in range(0, len(contour), 10):
         vx, vy, x, y = cv2.fitLine(contour[index : index + 10], cv2.DIST_L2, 0, 0.01, 0.01)
         vx, vy, x, y = float(vx), float(vy), float(x), float(y)
@@ -43,7 +47,6 @@ def determine_lines_for_contour(contour):
             dict(
                 {
                     "points": [(x, y)],
-                    #"mean_point": (x, y),
                     "vx": vx,
                     "vy": vy
                 }
@@ -68,12 +71,8 @@ def determine_lines_for_contour(contour):
         # stores the group to which the line matches best
         temp = dict({}) 
         # check every group 
-        # the distance between the y of the line and the y (for the same x) of the group is calculated
-        # if the distance (error) is small enough (<10) than the line is considered to match the group
-        # the distance is calculated in both ways (from the line to group and from group to the line)
-        # and the mean value is taken as the final distance
         for labeled_line in labeled_group:
-            min_distance, labeled = check_group_for_line(line, labeled_line, min_distance)
+            min_distance, labeled = check_group_for_line(line, labeled_line, min_distance, 5)
             if labeled:
                 temp = labeled_line
         # if the line did not match any group, it starts to represent a new group
@@ -108,87 +107,42 @@ def determine_lines_for_contour(contour):
             else:
                 horizontal_lines.append(labeled_line)
 
-    """ for line_group in vertical_lines:
-        for line_pair in line_group["points"][1:]:
-            cv2.circle(frame, (int(line_pair[0]), int(line_pair[1])), 2, (255,255,255), -1)
-            #cv2.circle(frame, (int(line_pair[1]), int(line_pair[1])), 2, (255,255,255), -1)
-        cv2.imshow("binary frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        input() """
     return vertical_lines, horizontal_lines
-    # here comes code for lines pairing, not sure if this process is necessary
-    """ line_pairs = []
-    start_index = 0
-    indices_taken = []
-
-    # for every line in the group we try to find the corresponding pair (the other/parallel side of the rectangle)
-    # since on this stage of the algorithm we expect to have only shpaes that remaind rectangles
-    for labeled_line in labeled_group:
-        start_index += 1
-        if labeled_group.index(labeled_line) not in indices_taken:
-            paired = False
-            d_min = sys.maxsize
-            length = len(labeled_line["points"][1:])
-            paired_index = -1
-            for index in range(start_index, len(labeled_group)):
-                if index not in indices_taken:
-                    n = (labeled_group[index]["points"][0][1] - labeled_line["points"][0][1]) / labeled_line["vy"]
-                    x = labeled_line["points"][0][0] + n * labeled_line["vx"]
-                    d = abs(x - labeled_group[index]["points"][0][0])
-                    a1 = labeled_line["vy"] / labeled_line["vx"]
-                    a2 = labeled_group[index]["vy"] / labeled_group[index]["vx"]
-                    length2 = len(labeled_group[index]["points"][1:])
-                    # we consider lines to be pairs when their slope and length are similar
-                    # as well as we choose the closest ones
-                    if abs(a1 - a2) < 0.5 and abs(length - length2) < 5 and d < d_min:
-                        d_min = d
-                        paired = True
-                        paired_index = index
-            if paired:
-                indices_taken.append(paired_index)
-                line_pairs.append((labeled_line, labeled_group[paired_index]))
-
-    for line_pair in line_pairs:
-        cv2.circle(frame, (int(line_pair[0]["points"][0][0]), int(line_pair[0]["points"][0][1])), 2, (255,255,255), -1)
-        cv2.circle(frame, (int(line_pair[1]["points"][0][0]), int(line_pair[1]["points"][0][1])), 2, (255,255,255), -1)
-        cv2.imshow("binary frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-        input("INPUT") 
-    return line_pairs
-    """
 
 def group_dashed_lines(lines):
     start_index = 0
     indices_taken = []
     groups_to_be_removed = []
+    # for every line (group) we check if it can be merged to other line (group)
     for line in lines:
         start_index += 1
         if line["vx"] == 0:
             continue
+        # check if the line with which we want to marge is not already marged
         if lines.index(line) not in indices_taken:
             min_distance = sys.maxsize
             temp = dict({})
-            for index in range(start_index, len(lines)):
-                if index not in indices_taken:
-                    min_distance, labeled = check_group_for_line(line, lines[index], min_distance)
+            for index in range(0, len(lines)):
+                # check if the line we want to merged to was not previously merged
+                if index not in indices_taken and lines[index] is not line:
+                    min_distance, labeled = check_group_for_line(line, lines[index], min_distance, 10)
                     if labeled:
                         temp = lines[index]
             if min_distance != sys.maxsize:
-                indices_taken.append(lines.index(temp))
-                groups_to_be_removed.append(temp)
-                line["points"] += temp["points"][1:]
+                indices_taken.append(lines.index(line))
+                groups_to_be_removed.append(line)
+                temp["points"] += line["points"][1:]
+                 # if we have merged two lines/groups, we recalculate the mean point of the group
                 first_point = np.array([temp["points"][0][0], temp["points"][0][1]], dtype=float)
                 second_point = np.array([line["points"][0][0], line["points"][0][1]], dtype=float)
                 points = np.array([first_point, second_point])
                 vx, vy, x, y = cv2.fitLine(points, cv2.DIST_L2, 0, 0.01, 0.01)
                 vx, vy, x, y = float(vx), float(vy), float(x), float(y)
-                line["points"][0] = (x, y)
-                line["vx"] = vx
-                line["vy"] = vy
-    
+                temp["points"][0] = (x, y)
+                temp["vx"] = vx
+                temp["vy"] = vy
+
+    # remove lines that were merged to other lines
     for group in groups_to_be_removed:
         lines.remove(group)
 
@@ -239,8 +193,6 @@ def find_trajectory(lines):
         cv2.circle(frame, (int(x_right), int(y_middle)), 10, (0, 0, 255), -1)
         cv2.line(frame, (int(x_top), 0), (int(x_bottom), frame.shape[0]), (255, 0, 0), 2) 
 
-#x = cv2.createHausdorffDistanceExtractor()
-
 while(video_read_correctly):
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -261,15 +213,26 @@ while(video_read_correctly):
 
             vertical_lines += determine_lines_for_contour(contour)[0]
 
-    vertical_lines = group_dashed_lines(vertical_lines)
+    group_dashed_lines(vertical_lines)
 
     find_trajectory(vertical_lines)
     cv2.imshow("binary frame", frame)
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(40) & 0xFF == ord('q'):
         break
 
     video_read_correctly, frame = video.read()
     
 video.release()
 cv2.destroyAllWindows()
+
+
+#print groups
+""" for line_group in vertical_lines:
+        for line_pair in line_group["points"][1:]:
+            cv2.circle(frame, (int(line_pair[0]), int(line_pair[1])), 2, (255,255,255), -1)
+            #cv2.circle(frame, (int(line_pair[1]), int(line_pair[1])), 2, (255,255,255), -1)
+        cv2.imshow("binary frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        input() """
