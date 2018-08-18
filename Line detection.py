6a95477cbe57fff703defe17b201dfb68af8c0d4
maniusz28@wp.pl <maniusz28@wp.pl>
2018-08-18 15:34:34 +0200

    line detection alg finished, displacement alg to be done

--- Line detection.py
+++ Line detection.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 import sys
 import math
+import time
 
 #video = cv2.VideoCapture(0) ----- for camera read
-video = cv2.VideoCapture("Cut films/5.mp4")
+video = cv2.VideoCapture("Cut films/1.mp4")
 
 cv2.namedWindow('binary frame')
-cv2.createTrackbar('H', 'binary frame', 20, 174, lambda x: None)
+cv2.createTrackbar('H', 'binary frame', 24, 174, lambda x: None)
 cv2.setTrackbarMin('H', 'binary frame', 5)
 
 video_read_correctly, frame = video.read()
@@ -64,14 +65,13 @@ def fit_line(fit_to, save_to):
 
 def determine_lines_for_contour(contour, batch_size = -1):
     lines = []
-    # fits line to every 10 points of the contour
+    # fits line to every 10% of the points of the contour
+    # or every 2 points if the contour length is < 20
     contour_length = len(contour)
     if batch_size == -1:
         batch_size = 2
-        if 25 < contour_length:
-            batch_size = int(0.08 * contour_length)
-        #elif contour_length >= 150:
-            #batch_size = 15
+        if contour_length > 13:
+            batch_size = int(0.16 * contour_length)
     for index in range(0, contour_length - 1, batch_size):
         points = []
         if contour_length - (index + batch_size) != 2:
@@ -94,48 +94,41 @@ def determine_lines_for_contour(contour, batch_size = -1):
     return lines
 
 def group_dashed_lines(lines, divide_into_subsets):
-    groups_to_be_removed = []
+    to_be_removed = []
     # for every line (group) we check if it can be merged to other line (group)
     for line in lines:
         # check if the line with which we want to marge is not already marged
-        if line not in groups_to_be_removed:
+        if line not in to_be_removed:
             min_distance = sys.maxsize
             temp = dict({})
             for index in range(0, len(lines)):
                 # check if the line we want to merge to was not previously merged
-                if lines[index] not in groups_to_be_removed and lines[index] is not line: 
+                if lines[index] not in to_be_removed and lines[index] is not line: 
                     min_distance, labeled = check_group_for_line(line, lines[index], min_distance, 5)
                     if labeled:
                         temp = lines[index]
             if min_distance != sys.maxsize:
-                groups_to_be_removed.append(line)
+                to_be_removed.append(line)
                 temp["points"] += line["points"]
                 temp["original"] = np.append(temp["original"], line["original"], axis=0)
                  # if we have merged two lines/groups, we recalculate the mean point of the group
-                first_point = np.array([temp["points"][0][0], temp["points"][0][1]], dtype=np.int32)
-                second_point = np.array([line["points"][0][0], line["points"][0][1]], dtype=np.int32)
-                points = np.array([first_point, second_point])
-                #p1, p2 = find_extreme_points(temp)
-                #points = np.array([p1, p2])
                 fit_line(temp["original"], temp)
-                #fit_line(np.array(points, dtype=float), temp)
 
     # remove lines that were merged to other lines
-    for group in groups_to_be_removed:
+    for group in to_be_removed:
         lines.remove(group)
 
     if divide_into_subsets:
         new_lines = []
-        groups_to_be_removed = []
+        to_be_removed = []
 
         for line in lines:
-            if len(line["points"]) == 1 and line["original"].shape[0] >= 6:
-                groups_to_be_removed.append(line)
+            if len(line["points"]) == 1 and line["original"].shape[0] >= 4:
+                to_be_removed.append(line)
                 new_lines += determine_lines_for_contour(line["original"], int(line["original"].shape[0] / 2))
-        # remove groups consisting of only one point
-        #lines = [line for line in lines if len(line["points"]) > 1]
-        for group in groups_to_be_removed:
-            lines.remove(group)
+
+        for line in to_be_removed:
+            lines.remove(line)
 
         if len(new_lines) > 0:
             lines += new_lines
@@ -161,7 +154,7 @@ def find_trajectory(lines):
             x_right = x
             line_right = line
     # if both left and right lines are present
-    # we calculate the value of x in the middle og=f the frame height
+    # we calculate the value of x in the middle of the frame height
     # then we take the middle point between x from right and left
     # we change the vy vector to be pointing the positive y direction
     # then we calculate the resultant vector which is the direction for the trajectory
@@ -188,49 +181,143 @@ def find_trajectory(lines):
         cv2.circle(frame, (int(x_right), int(y_middle)), 10, (0, 0, 255), -1)
         cv2.line(frame, (int(x_top), 0), (int(x_bottom), frame.shape[0]), (255, 0, 0), 2) 
 
-def find_extreme_points(line):
+def find_extreme_points(points):
+    max_x = -sys.maxsize
+    min_x = sys.maxsize
     max_y = -sys.maxsize
     min_y = sys.maxsize
-    x1 = 0
-    x2 = 0
-    for point in line["original"]:
+    for point in points:
+        if point[0][0] > max_x:
+            max_x = point[0][0]
+        if point[0][0] < min_x:
+            min_x = point[0][0]
         if point[0][1] > max_y:
             max_y = point[0][1]
-            x1 = point[0][0]
         if point[0][1] < min_y:
             min_y = point[0][1]
-            x2 = point[0][0]
-    return (x1, max_y), (x2, min_y)
+    return min_x, max_x, min_y, max_y
 
 def draw_line(line):
     for index in range(0, line["original"].shape[0]):
         cv2.circle(frame, (line["original"][index][0][0], line["original"][index][0][1]), 2, (255, 0, 0), -1)
-        #(line["original"][index + 1][0][0], line["original"][index + 1][0][1])
+
+def filter_small_lines(lines):
+    to_be_removed = []
+    for line in lines:
+        min_x, max_x, min_y, max_y = find_extreme_points(line["original"])
+        if max_x - min_x < frame.shape[1] / 20 and max_y - min_y < frame.shape[0] / 20:
+            to_be_removed.append(line)
+    for line in to_be_removed:
+        lines.remove(line)
+
+def filter_not_paired_lines(lines):
+    not_paired = []
+    # for every line in the group we try to find the corresponding pair (the other/parallel side of the rectangle)
+    # since on this stage of the algorithm we expect to have only shpaes that remaind rectangles
+    for line in lines:
+        paired = False
+        for index in range(0, len(lines)):
+            if lines[index] is not line and lines[index] not in not_paired:
+                if line["vy"] == 0:
+                    line["vy"] = 0.00001
+                n = (lines[index]["points"][0][1] - line["points"][0][1]) / line["vy"]
+                x = line["points"][0][0] + n * line["vx"]
+                d = abs(x - lines[index]["points"][0][0])
+                a1 = line["vy"] / line["vx"]
+                a2 = lines[index]["vy"] / lines[index]["vx"]
+                # we consider lines to be pairs when their slope and length are similar
+                # as well as we choose the closest ones
+                if abs(a1 - a2) < 0.5 and d < 20:
+                    paired = True
+                    break
+        if not paired:
+            not_paired.append(line)
+
+    for line in not_paired:
+        lines.remove(line)
+
+    return not_paired
+
+def compute_translation(first_set, second_set):
+    #using Kabsch algorithm: https://en.wikipedia.org/wiki/Kabsch_algorithm
+    first_centroid = np.mean(first_set, axis=0)
+    second_centroid = np.mean(second_set, axis=0)
+    first_set_centered = first_set - first_centroid
+    second_set_centered = second_set - second_centroid
+    cross_covariance_matrix = np.matmul(np.transpose(first_set_centered), second_set_centered)
+    u, s, vh = np.linalg.svd(cross_covariance_matrix)
+    d = np.linalg.det(np.matmul(np.transpose(vh), np.transpose(u)))
+    rotation = np.matmul(np.matmul(np.transpose(vh), np.array([[1, 0], [0, d]])), np.transpose(u))
+    translation = second_centroid - np.matmul(rotation, first_centroid)
+    return rotation, translation
 
 
+
+next_frame = None
+#bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+orb = cv2.ORB_create()
+kp1, des1 = orb.detectAndCompute(frame, None)
+FLANN_INDEX_LSH = 6
+index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 12,
+                   key_size = 20,     
+                   multi_probe_level = 2)
+search_params = dict(checks=50)
+flann_matcher = cv2.FlannBasedMatcher(index_params, search_params)
+displ_x = 0
+displ_y = 0
 while(video_read_correctly):
+    #start = time.process_time()
+    if next_frame is not None:
+        kp2, des2 = orb.detectAndCompute(next_frame,None)
+        matches = flann_matcher.match(des1,des2)
+        matches = sorted(matches, key = lambda x:x.distance)
+        first_set = []
+        second_set = []
+        for match in matches[:10]:
+            first_set += [kp1[match.queryIdx].pt]
+            second_set += [kp2[match.trainIdx].pt]
+        first_set = np.array(first_set, dtype=np.int32)
+        second_set = np.array(second_set, dtype=np.int32)
+        #print(kp1[0].pt)
+        #print(np.array([kp.pt for kp in kp1], dtype=np.int32))
+        matrices = compute_translation(first_set, second_set)
+        displ_x += matrices[1][0]
+        displ_y += matrices[1][1]
+        #print(find_centroid(kp2))
+        # Sort them in the order of their distance.
+        
 
+        # Draw first 10 matchesframe
+        img3 = cv2.drawMatches(frame, kp1, next_frame, kp2, matches[:10], outImg=np.array([]), flags=0)
+        #plt.imshow(img3),plt.show()
+        kp1 = kp2.copy()
+        des1 = des2.copy()
+        #current_frame = next_frame.copy()
+    #end = time.process_time()
+    #print(end - start)
+    #input()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     H = cv2.getTrackbarPos('H', 'binary frame')
-    lower_H = np.array([[[H - 5, 75, 50]]])
+    lower_H = np.array([[[H - 5, 75, 75]]])
     upper_H = np.array([[[H + 5, 255, 255]]])
         
     extracted_color_frame = cv2.inRange(hsv_frame, lower_H, upper_H)
 
-    _, contours, _ = cv2.findContours(extracted_color_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    lines = []
+    _, contours, _ = cv2.findContours(extracted_color_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
     
+    lines = []
     #cv2.drawContours(frame, contours, -1, (0, 0, 255), 1)
+    
     for contour in contours:
-
+        
         if cv2.contourArea(contour) >= 300:
-
+            
             temp = determine_lines_for_contour(contour)
-
+            
             new_lines = True
-
+            
             while new_lines:
                 temp, new_lines = group_dashed_lines(temp, True)
 
@@ -247,47 +334,37 @@ while(video_read_correctly):
     while number_of_groups > len(lines):
         number_of_groups = len(lines)
         lines, _ = group_dashed_lines(lines, False)
-    #lines = group_dashed_lines(lines)
-    lines = [line for line in lines if line["original"].shape[0] >= 6 or len(line["points"]) > 2]
 
+    lines = [line for line in lines if line["original"].shape[0] >= 6 or len(line["points"]) > 2]
+    filter_small_lines(lines)
+    
+    #input()
+    #filter_not_paired_lines(lines)
     for line in lines:
         #p1, p2 = find_extreme_points(line)
         #fit_line(np.array([[p1, p2]], dtype=np.int32), line)
         
         #print(line["original"][0][0])
         #cv2.line(frame, p1, p2, (255, 0, 0), 2) 
-        #find_extreme_points(line)
-        #print(line)
+
         draw_line(line)
         #for point in line["points"]:
             #cv2.circle(frame, (int(point[0]), int(point[1])), 2, (255, 255, 255), -1)
-        cv2.imshow("binary frame", frame)
+    cv2.imshow("binary frame", frame)
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
         #input()
-        if cv2.waitKey(40) & 0xFF == ord('q'):
-            break
-        input()
-        #print(type(contours))
-        #print((np.array([line["points"]], dtype=float)).shape)
+
+
         #cv2.drawContours(frame, [np.append(line["original"][0], line["original"][-1], axis=0)], -1, (0, 0, 255), 1, cv2.LINE_AA)
     #find_trajectory(lines)
-    #lines = group_dashed_lines(lines)
 
     video_read_correctly, frame = video.read()
+    #print(displ_x)
+    #print(displ_y)
+    if video_read_correctly:
+        next_frame = frame.copy()
 
 video.release()
 cv2.destroyAllWindows()
-
-
-#print groups
-""" for line in lines:
-        for points in line["points"][1:]:
-            cv2.circle(frame, (int(points[0]), int(points[1])), 2, (255,255,255), -1)
-            #cv2.circle(frame, (int(line_pair[1]), int(line_pair[1])), 2, (255,255,255), -1)
-        cv2.imshow("binary frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        #input() """
-
-# jesli jakas grupa nie zostala zgrupowana z zadna inna to podzielic ja na DWIE GRUPY i sprobowac jeszcze raz
-
-        #ORB
